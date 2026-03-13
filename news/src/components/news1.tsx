8fa90dd6411adfbec3aaebce9603733d289dfcf2
Akash Bhattacharyya <akashbhattacharyya45@gmail.com>
2026-03-13 12:10:47 +0530

    4

--- news/src/components/news1.tsx
+++ news/src/components/news1.tsx
@@ -57,7 +57,9 @@ return (
         <div className="">
             {articles.map((article, index) => (
                 <div className="" key={index}>
-                    
+                    <h3>{article.title}</h3>
+                    <p>{article.description}</p>
+                    <a href={article.url}>Read more</a>
                 </div>
             ))}
         </div>
