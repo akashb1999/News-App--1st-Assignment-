87f0a028fe8e295b4c1fd124e748420f65a75717
Akash Bhattacharyya <akashbhattacharyya45@gmail.com>
2026-03-13 23:44:00 +0530

    36 final

--- news/src/components/loader.tsx
+++ news/src/components/loader.tsx
@@ -13,7 +13,7 @@ function Loader() {
     if (loading) {
         return <h2 className="flex justify-center font-extrabold p-10 text-5xl">Loading...</h2>;
     }
-    return <div className="flex justify-center font-extrabold p-10 text-5xl>Loaded!, But there is nothing to show , //chek your Internet Concention!!!</div>;
+    return <div>Loaded!, But there is nothing to show , //chek your Internet Concention!!!</div>;
 };
 
 export default Loader;
