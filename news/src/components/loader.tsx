48bb50f5f739aa6e3b9aba6e5c1686296522a6b4
Akash Bhattacharyya <akashbhattacharyya45@gmail.com>
2026-03-14 14:17:41 +0530

    40

--- news/src/components/loader.tsx
+++ news/src/components/loader.tsx
@@ -14,9 +14,8 @@ function Loader() {
         return <>
         <h2 className="flex justify-center font-extrabold p-10 text-5xl m-20 ">Loading...</h2>;
         <div className="flex justify-center items-center">
-      <div className="h-20 w-20 border-4 border-t-blue-500 border-r-red-500 border-b-yellow-400 border-l-green-500 rounded-full animate-spin m-20"></div>
+        <div className="h-20 w-20 border-4 border-t-blue-500 border-r-red-500 border-b-yellow-400 border-l-green-500 rounded-full animate-spin m-20"></div>
     </div>
-
         </>
     }
     return <div>Loaded!, But there is nothing to show , //chek your Internet Concention!!!</div>;
